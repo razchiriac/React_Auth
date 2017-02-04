870a5f778dacaac0d0ccae48e0a97974d7b5cc6a
Raz <raz.chiriac@gmail.com>
2017-02-04 14:00:59 -0500

    Added Stormpath SDK

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import { IndexRoute, Route, browserHistory } from 'react-router';
+import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
+
+// Initialize the Stormpath SDK
+ReactStormpath.init();
 
 ReactDOM.render(
   <Router history={browserHistory}>
