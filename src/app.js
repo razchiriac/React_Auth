5c6bda4e7c6dbee578daf1490ac26059e748e984
Raz <raz.chiriac@gmail.com>
2017-02-04 13:57:01 -0500

    add react
    Added react to the project and set up a starting point for the React App

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+  </Router>,
+  document.getElementById('app-container')
+);
